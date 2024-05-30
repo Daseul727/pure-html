e9cab7864c9c583144d914bb63811f30d5a679a2
김다슬 <daseul727@hankookn.com>
2024-05-30 14:55:46 +0900

    portfolio - init basic project

---
+++ js/portfolio/main.js
@@ -0,0 +1,53 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.title = MAIN_TITLE;
+
+  document.getElementById("nav_menu_1").text = NAV_MENU_1
+  document.getElementById("nav_menu_2").text = NAV_MENU_2
+  document.getElementById("nav_menu_3").text = NAV_MENU_3
+  document.getElementById("nav_menu_4").text = NAV_MENU_4
+});
+
+
+/**
+ * add header scroll class
+ */
+$(window).scroll(function(){
+  var scroll = $(window).scrollTop();
+  if (scroll >= 50){
+    $('.header').addClass('scroll')
+  }else{
+    $('.header').removeClass('scroll')
+  }
+});
+
+/**
+ * nav click event
+ * @param id
+ */
+function navMove(id){
+  var offset = $("#" + id).offset().top;
+  if (window.innerWidth < 768) {
+    $('html, body').animate({scrollTop : offset - 70}, 400);
+  }else{
+    $('html, body').animate({scrollTop : offset}, 400);
+  }
+}
+
+new Swiper('.swiper-001', {
+
+  //자동
+  autoplay: {
+    delay: 4000
+  },
+  loop: true,
+
+  //보여줄 화면
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination-001',
+    clickable: true
+  }
+})
